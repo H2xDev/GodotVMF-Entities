9d71ff8c72564aa639f0c345482b7f97ea527b1a
Radik <h2xdeveloper@gmail.com>
2025-09-08 03:26:08 +0400

    Merge all func_occluder instances during import

--- entities/func_occluder.gd
+++ entities/func_occluder.gd
@@ -1,14 +1,31 @@
+## This entity will merge all func_occluder instances into one during import
+
 @tool
 class_name func_occluder extends ValveIONode
 
+var occluder_instance: OccluderInstance3D:
+	get: return get_node("occluder") as OccluderInstance3D;
+
 ## Entity setup method. Called during the map import process. Do additional setup for the entity here.
-func _apply_entity(entity: Dictionary) -> void:
-	super._apply_entity(entity);
+func _apply_entity(_entity: Dictionary) -> void:
+	super._apply_entity(_entity);
 
-	var occluder := get_node("occluder") as OccluderInstance3D;
-	var shape := ArrayOccluder3D.new();
+	var existing_occluder := get_parent().get_node_or_null("occluder") as func_occluder;
+	name = "occluder";
 
-	var vertices := get_entity_trimesh_shape().get_faces();
+	var occluder := occluder_instance if existing_occluder == null \
+		else existing_occluder.occluder_instance;
+
+	var shape := ArrayOccluder3D.new() if occluder.occluder == null \
+		else occluder.occluder as ArrayOccluder3D;
+
+	var new_vertices := get_entity_trimesh_shape().get_faces();
+	var vertices := shape.vertices;
+	vertices.append_array(new_vertices);
+
 	shape.set_arrays(vertices, range(0, vertices.size()));
 	occluder.occluder = shape;
 
+	if existing_occluder != null:
+		queue_free();
+		return;
