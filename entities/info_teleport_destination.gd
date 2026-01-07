99eb3f9e1c98d2e4eca8435763733067e4b14ec0
yourmom468x <yourmom55543@hotmail.com>
2026-01-06 18:06:57 -0800

    tele des angle fix
    angle 0 0 0 would always face 0 90 0

--- entities/info_teleport_destination.gd
+++ entities/info_teleport_destination.gd
@@ -20,14 +20,11 @@ func get_destination_transform() -> Transform3D:
 	transform.origin = destination_position
 	
 	# Use entity.angles and convert_direction like point_teleport does
+	# Always apply rotation (even if angles is 0 0 0) to match point_teleport behavior
 	var angles = entity.get("angles", Vector3.ZERO)
-	if angles != Vector3.ZERO:
-		# Convert angles using convert_direction (same as point_teleport)
-		var direction = convert_direction(angles)
-		# Set rotation Y (yaw) like point_teleport does: convert_direction(angles).y - PI / 2.0
-		transform.basis = Basis.from_euler(Vector3(0, direction.y - PI / 2.0, 0))
-	else:
-		# Fallback to node rotation if angles not set
-		transform.basis = Basis.from_euler(rotation_degrees * PI / 180.0)
+	# Convert angles using convert_direction (same as point_teleport)
+	var direction = convert_direction(angles)
+	# Set rotation Y (yaw) like point_teleport does: convert_direction(angles).y - PI / 2.0
+	transform.basis = Basis.from_euler(Vector3(0, direction.y - PI / 2.0, 0))
 	
 	return transform
